47032cc2755cedc15643a67b049814cda3a11a43
jejea <157323284+Jeje0001@users.noreply.github.com>
2026-09-16 20:32:53 -0400

    First Push

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-student.
+from student import Student
+
+s1=Student("Jeje",20,"Freshaman")
+s2=Student("AMY",10,"Sophmore")
+s1.courses.append("Math")
+s2.courses.append("English")
+print(s1.name)
+print(s2.name)
---
+++ student.py
@@ -0,0 +1,8 @@
+class Student:
+    school="Indiana University"
+
+    def __init__(self,name,age,year):
+        self.name=name
+        self.age=age
+        self.year=year
+        self.courses=[]
